a27562c0c2c7c95c28c4469d8898e8f53171cc10
sidaty <sidatycheriv@gmail.com>
2026-04-16 11:37:02 +0200

    I update 3 files .gitignore package.json server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname , 'dist/admin-dashboard/browser')));
+
+app.get('/',(req , res)=>{
+   res.sendFile(path.join(__dirname , 'dist/admin-dashboard/browser/index.html'))
+})
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
