f8d50331ac2579194364bd5a251274085aba142c
sidaty <sidatycheriv@gmail.com>
2026-04-16 14:07:23 +0200

    I modify apiUrl to ngrok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   // apiUrl: '/api',
-  apiUrl: 'http://localhost:8080/api',
+  // apiUrl: 'http://localhost:8080/api',
+   apiUrl: 'https://nonfervently-unlexicographical-amy.ngrok-free.dev/api',
   //apiUrl:'https://constrict-replace-pajamas.ngrok-free.dev/api'
 };
